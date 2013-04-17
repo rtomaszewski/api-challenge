c38f724d0e3c9dbc7cae51d09d7e74b0db31ca35
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2013-04-17 23:23:22 +0100

    - fixed spelling mistakes - implemented the show functions - implemented cloning

--- challenge2.py
+++ challenge2.py
@@ -17,7 +17,7 @@ import pyrax
 # sys.path.append(os.path.dirname(__file__))
 # print sys.path
 
-from challenge1 import Challange1
+from challenge1 import Challenge1
 
 DEBUG = 1
 PROGRAM_NAME="challenge2.py"
@@ -30,22 +30,23 @@ def debug(message):
     if DEBUG>0:
         log("[debug %2d]" % DEBUG + " " + message)
 
-class Challange2(Challange1):
+class Challenge2(Challenge1):
     """ 
     Challenge 2: Write a script that clones a server (takes an image and deploys 
     the image as a new server). Worth 2 Point
     """
 
     def __init__ (self, MAX_IMAGES=None):
-        Challange1.__init__(self, 1)
+        Challenge1.__init__(self, 1)
 
         # self.MAX_IMAGES=self.MAX_SERVERS
 
-        self.images=[]    
+        self.images=[]
+        self.images_obj=[ None for i in self._cs_range() ]
         self.images_build_status=[ 0 for i in self._cs_range() ]
 
-        self.clone_servers=[]    
-        self.clone_servers_build_status=[ 0 for i in self._cs_range() ]
+        self.cloned_servers=[]    
+        self.cloned_servers_build_status=[ 0 for i in self._cs_range() ]
 
         self.opt_delete_clones=True
         self.opt_delete_images=True
@@ -86,27 +87,35 @@ class Challange2(Challange1):
     def show(self):
         debug("show start")
         
-        Challange1.show()
+        log ("Created cloud server details:") 
+        self.show_cs(self.servers)
 
-        self.show_clones()
+        log ("Created images details:") 
         self.show_images()
 
-    def show_images(self):
+        log ("Created cloned server details:") 
+        self.show_clones()
+
+    def show_images(self, image_list=None):
         debug("show_images start")
 
-        log ("Created cloud server images details:") 
+        if not image_list : image_list=self.images_obj
 
-    def show_clones(self):
+        for i in self._cs_range() :
+          img=image_list[i]
+          print ("Image  #%2d:  ID %37s name %16s from server %s" % (i, img.id, img.name, img.server["id"]) )
+
+    def show_clones(self, servers_list=None):
         debug("show_clones start")
 
-        log ("Created cloned cloud server details:") 
-        self._show(self.clone_servers)
+        if not servers_list : servers_list=self.cloned_servers
+        self.show_cs(servers_list)
 
     def find_cloud(self, id):
         try:
             s=self.cs.servers.get(id)
             for i in self._cs_range():
-                self.clone_servers.append(s)
+                self.cloned_servers.append(s)
 
         except Exception, e:
             log("couldn't find cloud server id")
@@ -117,19 +126,51 @@ class Challange2(Challange1):
 
         for i in self._cs_range(count) :
             s=self.servers[i]
-            img=self.images[i]
+            img=self.images_obj[i]
+
+            t=datetime.datetime.now()
+            # '4.16.22.58.29'
+            # d.m.h.m.s
+            date_str=".".join(map (str, [t.month,t.day,t.hour,t.minute,t.second]))
 
             name=s.name
-            name_new=name+"-cs_from_"+img.name
+            name_new=name + "_" +date_str
 
             log("building new %s cloud server from image %s" % (name_new, img.name) )
             
-            s = self.cs.servers.create(name, img.id, self.flavor_512)
-            self.clone_servers.append(s)
+            new_s = self.cs.servers.create(name_new, img.id, self.flavor_512)
+            self.cloned_servers.append(new_s)
+
+    def check_one_clone(self, nr):
+        debug("check_one_clone start")
+
+        debug("checking %d" % nr)
+        
+        clone_id=self.current_items[nr]
+        i=self.cs.images.get(clone_id)
+        self.images_obj[nr]=i
+
+        debug("checking %d status %s progress %d " % (nr, i.status, i.progress))
+
+        if i.status == "ACTIVE":
+          return True
+        
+        return False
 
     def wait_for_cloning(self):
         debug("wait_for_cloning start")
-        log("msg wait_for_cloning start")
+        
+        start=datetime.datetime.now()
+        self.set_max_timeout()
+        self.sleep()
+
+        ret=self._wait_for_tasks(self.check_one_clone)
+
+        stop=datetime.datetime.now()
+        delta= stop - start
+        debug("waited for %s" % delta)
+        
+        return ret
 
     def clone(self):
         debug("clone start")
@@ -166,7 +207,7 @@ class Challange2(Challange1):
         if self.opt_delete_clones is False :
             return
     
-        self._delete_servers(self.clone_servers) 
+        self.delete_servers(self.cloned_servers) 
 
     def run(self):
         debug("main start")
@@ -216,14 +257,17 @@ class Challange2(Challange1):
         self.clone()
         
         log("Waiting for the clone images to be created ...")
+        self.set_current_build(self.images, self.images_build_status)    
         self.wait_for_cloning()
 
-        # log("Building %d new cloud server(s) from images." % self.MAX_SERVERS)
-        # self.build_servers_from_image()
-        # self.set_current_build(self.clone_servers, self.clone_servers_build_status)
-        # self.wait_for_build()
+        log("Building %d new cloud server(s) from taken images." % self.MAX_SERVERS)
+        self.build_servers_from_image()
+         
+        log("Waiting for the servers to be built from the cloned images ...")         
+        self.set_current_build(self.cloned_servers, self.cloned_servers_build_status)    
+        self.wait_for_build()
 
-        # self.show()
+        self.show()
 
         self.delete_servers()
         # self.delete_cloned_servers()
@@ -231,8 +275,8 @@ class Challange2(Challange1):
 
 if __name__ == '__main__': 
     # try:
-        chalange=Challange2()
-        chalange.run()
+        challenge=Challenge2()
+        challenge.run()
     # except Exception, e:
     #     chalange.delete_servers()
     #     # chalange.delete_cloned_servers()
