9d4334cb07c13c75f4156fa02c07e732529731ca
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2013-04-22 20:11:26 +0100

    - changed constructor - add check* functions - add create_domain func - add create_record func - add new option -d

--- challenge4.py
+++ challenge4.py
@@ -7,6 +7,7 @@ import getopt
 import json
 import re
 import datetime
+import socket
 
 from pprint import pprint
 from pprint import pformat
@@ -18,7 +19,7 @@ import base
 from base import ChallengeBase
 from base import log, debug
 
-DEBUG=0
+DEBUG = 0
 
 PROGRAM_NAME = "challenge4.py" 
 
@@ -28,60 +29,137 @@ class Challenge4(ChallengeBase):
     and IP address as arguments. 
     """
 
-    def __init__ (self, MAX_IMAGES=None, debug=0):
-        ChallengeBase.__init__(self,debug)
+    def __init__ (self, MAX_IMAGES=None, debug=0, args=[], optlist=[]):
+        ChallengeBase.__init__(self, debug)
 
         self.message ="""
-    usage: %s [-h] [-v] FQDN ip-add
+    usage: %s [-h] [-v] [-d] FQDN ip-add
         -h - usage help 
         -v - verbose / debug output 
+        -d - if a domain exists it will be deleted first 
+
         FQDN - Fully Qualified Domain Name; exaple www.wikipedia.org
         ip-add - ip address
     """ % (PROGRAM_NAME)
 
-        self.optlist, self.args = None, None
+        self.optlist = optlist
+        self.args = args
+        
+        self.opt_delete_domain = False
 
-
-    def check_path(self, path):
-        return os.path.isdir(path)
-
-    def check_container(self, container):
-        return bool(re.match('^[a-z0-9\\\\]+$', container, re.IGNORECASE))
-
-    def check(self, args):
-        debug("check start")
-        ret=True
+        if "-d" in map(lambda x: x[0], optlist) :
+            self.opt_delete_domain = True
 
         try:
-            self.path=args[0]
-            self.container=args[1]
+            self.domain_name = self.args[0]
+            self.domain = None
 
-            if self.check_path(self.path) == False: 
-                log("Invalid directory path string (check if the path directory exists and has alphanumeric characters only")
-                ret=False
-            
-            if self.check_container(self.container) == False:
-                log("Container name has invalid chars")
-                ret=False
+            self.ip_str = self.args[1]
 
         except Exception, e:
-            ret=False
             log("missing params")
+            self.usage()
+            sys.exit(-1)
 
+
+    def check_domain_name(self, domain_name):
+        return bool(re.match('^[a-z0-9.-]+$', domain_name, re.IGNORECASE))
+
+    def convert_ip(self, ip_str):
+        return socket.inet_ntoa( socket.inet_aton(ip_str) )
+
+    def check_ip(self, ip_str):
+        try:
+            socket.inet_aton(ip_str)
+        except socket.error:
+            return False
+
+        return True
+
+    def check(self):
+        debug("check start")
+        ret = True
+
+        if self.check_domain_name(self.domain_name) == False: 
+            log("Invalid domain name string")
+            ret = False
+        
+        if self.check_ip(self.ip_str) == False:
+            log("Invalid ip address")
+            ret = False
+        else :
+            self.ip_str = self.convert_ip(self.ip_str)
+            
         return ret
 
+    def create_domain(self, domain_name):
+        debug("create_domain start")
+        
+        try:
+            dom = self.dns.find(name=domain_name)
+            if self.opt_delete_domain :
+                debug("deleting domain %s" % domain_name)
+                dom.delete()
+            else:
+                log("Domain %s exists, please specify other domain name or use -d option" % domain_name)
+                return False
+
+        except exc.NotFound:
+            pass
+
+        try:
+            dom = self.dns.create(name=domain_name, emailAddress="sample@challenge4.edu",
+                    ttl=900, comment="challenge4 domain")
+        except exc.DomainCreationFailed as e:
+            log("Domain creation failed: %s" % e)
+            return False
+
+        self.domain = dom
+        log("Domain created: %s" % domain_name)
+
+        return True
+
+    def create_record(self, domain_name, ip_str) :
+        debug("create_record start")
+
+        # Substitute your actual domain name and IP addresses here
+        a_rec = {"type": "A",
+                "name": domain_name,
+                "data": ip_str,
+                "ttl": 6000
+                }
+
+        log("Adding records to our domain ...")
+        recs = self.domain.add_records([a_rec])
+
+        return True
+
+    def show(self):
+        debug("show start")
+
+        dom = self.dns.find(name=self.domain_name)
+
+        log("domain name: %s created at %s" % (dom.name, dom.created) )
+        for r in dom.list_records() : 
+            log("record %3s %s -> %s" % (r.type, r.name, r.data) )
 
     def run(self):
         debug("run start")
         ChallengeBase.run(self)
         
-        if self.check(args) is False:
+        if self.check() is False:
             self.usage()
             sys.exit(-1)
         
+        if self.create_domain(self.domain_name) == False:
+            self.usage()
+            sys.exit(-1)    
+
+        if self.create_record(self.domain_name, self.ip_str) :
+            self.show()
 
 if __name__ == '__main__': 
-    optlist, args = getopt.getopt(sys.argv[1:], 'vh')
+    optlist, args = getopt.getopt(sys.argv[1:], 'vhd')
 
     for o, val in optlist:
         if o == "-v":
@@ -95,5 +173,5 @@ if __name__ == '__main__':
             Challenge4(DEBUG).usage()
             sys.exit()
 
-    challenge = Challenge4(debug=DEBUG)
+    challenge = Challenge4(debug=DEBUG, args=args, optlist=optlist)
     challenge.run()
