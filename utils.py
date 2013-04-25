2a2285aa4b80e03960d892be47fe5b33b9d125d3
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2013-04-25 02:00:49 +0100

    new func

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-
+import re
 # cs.networks
 # {u'private': [u'10.178.197.30'],
 #  u'public': [u'95.138.180.58', u'2a00:1a48:7805:0111:8cfc:cf10:ff08:4465']}
@@ -12,3 +12,11 @@ def get_ipv4net(net_list):
             return net
 
     return None
+
+def get_image(cs):
+    [ image ] = filter ( lambda x : bool(re.match("Ubuntu 10.04.*" , x.name)), cs.images.list())
+    return image.id
+
+def get_flavor(cs):
+    [ f512 ] = filter( lambda x : x.ram==512 , cs.flavors.list())
+    return f512.id
